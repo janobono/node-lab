ed7e81debaf120450af24621ebf6f57b4f27b086
janobono <janobono@gmail.com>
2020-08-15 18:17:28 +0200

    traefik fix
    Signed-off-by: janobono <janobono@gmail.com>

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("/api/node-lab-backend/info")
   getHello(): string {
     return this.appService.getHello();
   }
