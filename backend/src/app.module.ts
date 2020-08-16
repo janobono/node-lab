c3f26e0bd9edb9a36eb760269d2644c714e55469
janobono <janobono@gmail.com>
2020-08-16 21:44:27 +0200

    import clean up
    Signed-off-by: janobono <janobono@gmail.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import {Module} from '@nestjs/common';
 import {ConfigModule} from "@nestjs/config";
 import configuration from './config/configuration';
+import {LoggerModule} from './logger/logger.module';
 import {InfoModule} from './info/info.module';
-import { LoggerModule } from './logger/logger.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({load: [configuration]}), InfoModule, LoggerModule],
+    imports: [ConfigModule.forRoot({load: [configuration]}), LoggerModule, InfoModule],
     controllers: [],
     providers: [],
 })
